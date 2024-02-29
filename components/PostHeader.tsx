c82fd8f17354b9b37a9a08b4f2c9af8bb3ac524b
Martin Bechard <clr.rtc@gmail.com>
2024-02-29 16:40:06 -0500

    - Fix problem with french menu on english pages - Added a title on each page for consistency - Fix layout issue if there is a cover image for a page defined in Sanity CMS

--- components/PostHeader.tsx
+++ components/PostHeader.tsx
@@ -8,57 +8,66 @@ import type { Post } from 'lib/sanity.queries'
 import NavButton from './NavButton'
 import TagButtonList from './TagButtonList'
 
-
 export default function PostHeader(
-  props: Pick<Post, 'title' |  'date' | 'coverImage' | 'author' | 'slug' | 'previousSlug' | 'nextSlug'>,
+  props: Pick<
+    Post,
+    | 'title'
+    | 'date'
+    | 'coverImage'
+    | 'author'
+    | 'slug'
+    | 'previousSlug'
+    | 'nextSlug'
+  >,
 ) {
-  const { title, coverImage, date, author, slug, previousSlug, nextSlug } = props
+  const { title, coverImage, date, author, slug, previousSlug, nextSlug } =
+    props
 
   return (
     <>
       <div className="flex">
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full">
           <PostTitle>{title}</PostTitle>
-          {author &&
-          <div className="mb-4 mt-4 sm:mb-6 md:block">
-            <Avatar name={author?.name} picture={author?.picture} />
-          </div>}
+          {author && (
+            <div className="mb-4 mt-4 sm:mb-6 md:block">
+              <Avatar name={author?.name} picture={author?.picture} />
+            </div>
+          )}
           <div className="max-w-2xl">
             <div className="mb-2 text-lg">
               <Date dateString={date} />
-              <TagButtonList keywords={props['keywords']} className=''/>
+              <TagButtonList keywords={props['keywords']} className="" />
             </div>
           </div>
-          {(previousSlug || nextSlug) &&
-          <PostNavBar prevSlug={previousSlug} nextSlug={nextSlug} />}
+          {(previousSlug || nextSlug) && (
+            <PostNavBar prevSlug={previousSlug} nextSlug={nextSlug} />
+          )}
         </div>
-        {coverImage &&
-        <div className="mt-4 mb-2 flow-right w-96">
-          <CoverImage title={title} image={coverImage} priority slug={slug} />
-        </div>}
+        {coverImage && (
+          <div className="mt-4 mb-2 pl-4 flow-right w-96">
+            <CoverImage title={title} image={coverImage} priority slug={slug} />
+          </div>
+        )}
       </div>
     </>
   )
-
 }
 
-function PostNavBar({prevSlug, nextSlug}) {
-
+function PostNavBar({ prevSlug, nextSlug }) {
   const prefix = useLangUri()
   const PREVIOUS = useLabel('Précédents', 'Previous')
   const NEXT = useLabel('Suivants', 'Next')
 
-
- return <><div className="flex flex-row py-1 mt-1 sm:mt-2 gap-x-2 ">
-    <NavButton
-      disabled={!prevSlug}
-      url={`${prefix}/posts/${prevSlug}`}>
-      &lsaquo;&nbsp;{PREVIOUS}</NavButton>
-    <NavButton
-      disabled={!nextSlug}
-      url={`${prefix}/posts/${nextSlug}`}>
-        {NEXT}&nbsp;&rsaquo;</NavButton>
-
-    </div>
-  </>
-  }
+  return (
+    <>
+      <div className="flex flex-row py-1 mt-1 sm:mt-2 gap-x-2 ">
+        <NavButton disabled={!prevSlug} url={`${prefix}/posts/${prevSlug}`}>
+          &lsaquo;&nbsp;{PREVIOUS}
+        </NavButton>
+        <NavButton disabled={!nextSlug} url={`${prefix}/posts/${nextSlug}`}>
+          {NEXT}&nbsp;&rsaquo;
+        </NavButton>
+      </div>
+    </>
+  )
+}
--- components/PostListPage.tsx
+++ components/PostListPage.tsx
@@ -1,4 +1,3 @@
-
 import Container from 'components/BlogContainer'
 import BlogHeader from 'components/BlogHeader'
 import Layout from 'components/BlogLayout'
@@ -7,16 +6,23 @@ import IndexPageHead from 'components/IndexPageHead'
 import StoriesList from 'components/StoriesList'
 import IntroTemplate from 'intro-template'
 import * as demo from 'lib/demo.data'
-import type { Post, Part, Settings, MenuItem, Keyword } from 'lib/sanity.queries'
+import type {
+  Post,
+  Part,
+  Settings,
+  MenuItem,
+  Keyword,
+} from 'lib/sanity.queries'
 import Link from 'next/link'
 
 import BlogPart from 'components/BlogPart'
 import StandardPageLayout from 'components/StandardPageLayout'
-import ListBanner  from './ListBanner'
+import ListBanner from './ListBanner'
 import { useLabel, useLangUri } from 'lib/lang'
 
 import NavButton from './NavButton'
 import PostText from './PostText'
+import PostHeader from './PostHeader'
 
 export interface PostListPageProps {
   preview?: boolean
@@ -37,75 +43,120 @@ export default function PostListPage(props: PostListPageProps) {
   const filterId = filter?._id
   const filterName = useLabel(filter?.title, filter?.title_en)
   const filterSummary = useLabel(filter?.themeSummary, filter?.themeSummary_en)
-  const filterDescription = useLabel(filter?.themeDescription, filter?.themeDescription_en)
+  const filterDescription = useLabel(
+    filter?.themeDescription,
+    filter?.themeDescription_en,
+  )
 
-  const filteredPosts =  posts.filter((p) =>  (!filterId || p.keywords?.find((kw) => kw._id === filterId)))
+  const filteredPosts = posts.filter(
+    (p) => !filterId || p.keywords?.find((kw) => kw._id === filterId),
+  )
 
-  const pagePosts = filteredPosts.slice((pageNo-1)*PAGE_SIZE, pageNo*PAGE_SIZE)
+  const pagePosts = filteredPosts.slice(
+    (pageNo - 1) * PAGE_SIZE,
+    pageNo * PAGE_SIZE,
+  )
 
   const { title = demo.title, description = demo.description } = settings || {}
 
-  const filterSuffix = filter? `/${encodeURIComponent(filter._id)}` : ''
+  const filterSuffix = filter ? `/${encodeURIComponent(filter._id)}` : ''
 
-  const ALL_ARTICLES = useLabel('Tous les articles','All Articles')
-  const THEME = useLabel('Enjeu Majeur','Key Issue')
-  const SEARCH = useLabel('Catégorie','Category')
+  const PAGE_TITLE = useLabel('Archives', 'Archives')
+  const ALL_ARTICLES = useLabel('Tous les articles', 'All Articles')
+  const THEME = useLabel('Enjeu Majeur', 'Key Issue')
+  const SEARCH = useLabel('Catégorie', 'Category')
   return (
     <>
       <IndexPageHead settings={settings} />
 
       <Layout preview={preview} loading={loading}>
         <Container>
-          <BlogHeader title={title} description={description} parts={parts} menuItems={props.menuItems} />
-         
+          <BlogHeader
+            title={title}
+            description={description}
+            parts={parts}
+            menuItems={props.menuItems}
+          />
+
           <StandardPageLayout parts={parts}>
-          <ListBanner highlight={true}>{filterName? ((filter.keywordType === 'theme'? THEME : SEARCH) + ": " + filterName) : ALL_ARTICLES}</ListBanner>
-          {filterDescription && <PostText content={filterDescription}/>}
-          {!filterDescription && filterSummary? <>{filterSummary}</> : <></>}
-          <NavBar />
-            {pagePosts.length > 0 && <div className="w-full pt-0 sm:pt-4">
-              <StoriesList posts={pagePosts} maxStories={PAGE_SIZE} noNavigation={true}/></div>}
+            <PostHeader title={PAGE_TITLE} />
+            <ListBanner highlight={true}>
+              {filterName
+                ? (filter.keywordType === 'theme' ? THEME : SEARCH) +
+                  ': ' +
+                  filterName
+                : ALL_ARTICLES}
+            </ListBanner>
+            {filterDescription && <PostText content={filterDescription} />}
+            {!filterDescription && filterSummary ? <>{filterSummary}</> : <></>}
+            <NavBar />
+            {pagePosts.length > 0 && (
+              <div className="w-full pt-0 sm:pt-4">
+                <StoriesList
+                  posts={pagePosts}
+                  maxStories={PAGE_SIZE}
+                  noNavigation={true}
+                />
+              </div>
+            )}
             <NavBar />
           </StandardPageLayout>
-            
         </Container>
-   
       </Layout>
     </>
   )
 
-  function pageUrl(pageNo: number){
+  function pageUrl(pageNo: number) {
     return `/postlist/${pageNo}${filterSuffix}`
   }
 
   function NavBar() {
-    const lastPageNo =filteredPosts.length? Math.floor((filteredPosts.length-1)/PAGE_SIZE) + 1 : 0
+    const lastPageNo = filteredPosts.length
+      ? Math.floor((filteredPosts.length - 1) / PAGE_SIZE) + 1
+      : 0
     const prefix = useLangUri()
     const PREVIOUS = useLabel('Précédents', 'Previous')
     const NEXT = useLabel('Suivants', 'Next')
-    
+
     const pages = []
-    if (lastPageNo > 1){
-      for (let i = 1; i <= lastPageNo; i++){
-        pages.push(<a key={i} href={pageNo === i? undefined : prefix + pageUrl(i)} className={"w-4 h-4 text-center " + (pageNo === i? 'bg-indigo-300' : 'bg-grey-300')} >{i}</a>)
+    if (lastPageNo > 1) {
+      for (let i = 1; i <= lastPageNo; i++) {
+        pages.push(
+          <a
+            key={i}
+            href={pageNo === i ? undefined : prefix + pageUrl(i)}
+            className={
+              'w-4 h-4 text-center ' +
+              (pageNo === i ? 'bg-indigo-300' : 'bg-grey-300')
+            }
+          >
+            {i}
+          </a>,
+        )
       }
     }
 
-   return <><div className="flex flex-row py-1 mt-1 sm:mt-2 gap-x-2 ">
-      <NavButton 
-        disabled={pageNo === 1} 
-        url={`${prefix}/postlist/${pageNo - 1}${filterSuffix}`}>
-        &lsaquo;&nbsp;{PREVIOUS}</NavButton>
-      <NavButton 
-        disabled={pageNo  >= lastPageNo} 
-        url={`${prefix}/postlist/${pageNo + 1}${filterSuffix}`}>
-          {NEXT}&nbsp;&rsaquo;</NavButton>
-          
+    return (
+      <>
+        <div className="flex flex-row py-1 mt-1 sm:mt-2 gap-x-2 ">
+          <NavButton
+            disabled={pageNo === 1}
+            url={`${prefix}/postlist/${pageNo - 1}${filterSuffix}`}
+          >
+            &lsaquo;&nbsp;{PREVIOUS}
+          </NavButton>
+          <NavButton
+            disabled={pageNo >= lastPageNo}
+            url={`${prefix}/postlist/${pageNo + 1}${filterSuffix}`}
+          >
+            {NEXT}&nbsp;&rsaquo;
+          </NavButton>
 
-      <div className="text-xs w-32  gap-1 sm:items-center flex flex-row flex-wrap justify-start h-full">
-        {pages}        
+          <div className="text-xs w-32  gap-1 sm:items-center flex flex-row flex-wrap justify-start h-full">
+            {pages}
+          </div>
         </div>
-      </div>
-    </>  
-    }
+      </>
+    )
+  }
 }
