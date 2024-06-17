41fe37e03d4f92be5ec2683a4361bef199594b2d
Martin Bechard <clr.rtc@gmail.com>
2024-06-17 03:07:14 -0400

    Use reduced image sizes for returning to client

--- pages/api/rgd/[id]/index.ts
+++ pages/api/rgd/[id]/index.ts
@@ -1,5 +1,5 @@
-import { kv } from '@vercel/kv'
 import { NextApiRequest, NextApiResponse } from 'next'
+import sharp from 'sharp'
 
 function parseBool(val: string) {
   if (!val) {
@@ -50,6 +50,7 @@ async function retrieveFile(res, id) {
   const fileUrl = `https://drive.google.com/uc?export=download&id=${id}`
 
   try {
+    //res.send(buffer)
     const response = await fetch(fileUrl)
     if (!response.ok) {
       throw new Error('Failed to fetch the image')
@@ -58,13 +59,37 @@ async function retrieveFile(res, id) {
     // Get the image content type
     const contentType = response.headers.get('content-type')
 
+    // Ensure the content type is an image
+    if (!contentType.startsWith('image/')) {
+      throw new Error('The fetched file is not an image')
+    }
+
     // Get the image buffer
-    const buffer = Buffer.from(await response.arrayBuffer())
+    const arrayBuffer = await response.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+
+    // Resize the image if it exceeds 4MB
+    const maxSize = 4 * 1024 * 1024 // 4MB
+    let resizedBuffer = buffer
+
+    if (buffer.length > maxSize) {
+      resizedBuffer = await sharp(buffer)
+        .rotate() // Automatically rotate the image based on EXIF data
+        .resize({
+          width: 1920, // Example width, adjust as needed
+          height: 1080, // Example height, adjust as needed
+          fit: sharp.fit.inside,
+          withoutEnlargement: true,
+        })
+        .toBuffer()
+    }
 
     // Set the appropriate headers and return the image
     res.setHeader('Content-Type', contentType)
     res.setHeader('Content-Disposition', 'inline')
-    res.send(buffer)
+    res.setHeader('Content-Length', resizedBuffer.length)
+
+    res.send(resizedBuffer)
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
